2b054a9879e32677dfd873351d2ce9b46bf7f69e
Zenas Salik <zsalik@gmail.com>
2020-02-13 17:43:54 -0500

    commit project

---
+++ Notes/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Notes.Data;
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        static NoteDatabase database;
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+            //Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            //Handle when app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            //Handle when app resumes
+        }
+    }
+}
